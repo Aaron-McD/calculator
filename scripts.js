153db36efb382dbbbc09d0b99b3d5f3990cf2e57
Aaron <aaronmcdonald3d@gmail.com>
2020-05-17 13:45:31 -0400

    Final touches to make the calculator respond to keyboard input

--- scripts.js
+++ scripts.js
@@ -1,69 +1,124 @@
 const numberButtons = document.querySelectorAll('.number-button');
 const operationButtons = document.querySelectorAll('.operation');
 const inputOutput = document.querySelector('#input-output');
+const htmlDoc = document.querySelector('html');
 let globalOperator = undefined;
 let operand1 = undefined;
 let operand2 = undefined;
 let operationFlag = false;
 let total = 0;
+let keyPressed = false;
 
-numberButtons.forEach((button) => {
-    button.addEventListener('click', (e) => {
+function numberClicked(e) {
+    if(keyPressed){
+        newText = e.key;
+        keyPressed = false;
+    } else {
         newText = e.target.textContent;
-        if(newText == ".") {
-            if(inputOutput.textContent.indexOf('.') >= 0) {
-                return;
-            } 
+    }   
+    if(newText == ".") {
+        if(inputOutput.textContent.indexOf('.') >= 0) {
+            return;
+        } 
+    }
+    if(operationFlag) {
+        inputOutput.textContent = '';
+        operationFlag = false;
+    }
+    inputOutput.textContent += newText;
+}
+
+function operationClicked(e) {
+    let newNum = parseFloat(inputOutput.textContent);
+    let itemClicked = '';
+    if(keyPressed) {
+        keyPressed = false;
+        switch(e.keyCode) {
+            case 111:
+                itemClicked = 'divide';
+                break;
+            case 106:
+                itemClicked = 'multiply';
+                break;
+            case 109:
+                itemClicked = 'subtract';
+                break;
+            case 107:
+                itemClicked = 'add';
+                break;
+            case 13:
+                itemClicked = 'equals';
+                break;
+            case 27:
+                itemClicked = 'clear';
+                break;
         }
-        if(operationFlag) {
-            inputOutput.textContent = '';
-            operationFlag = false;
+    } else {
+       itemClicked = e.target.id; 
+    }
+    if(itemClicked == 'clear'){
+        inputOutput.textContent = "";
+        globalOperator = undefined;
+        operand1 = undefined;
+        operand2 = undefined;
+    } else if(newNum != newNum) {
+        alert("Enter a number before trying an operation");
+    } else {
+        if(itemClicked == 'negate') {
+            inputOutput.textContent = negate(newNum);
+        } else if(e.target.id == 'one-over-x') {
+            inputOutput.textContent = oneOverX(newNum);
+        } else if(e.target.id == 'square-root') {
+            inputOutput.textContent = squareRoot(newNum);
+        } else if(itemClicked == 'equals') {
+            if(operand1 != undefined) {
+                operand2 = newNum;
+                total = operate(operand1, operand2, globalOperator);
+                inputOutput.textContent = total;
+                operationFlag = true;
+                operand1 = undefined;
+                operand2 = undefined;
+            }
+        } else {
+            if(operand1 == undefined) {
+                operand1 = newNum;
+                globalOperator = itemClicked;
+                inputOutput.textContent = '';
+            } else {
+                operand2 = newNum;
+                operand1 = operate(operand1, operand2, globalOperator);
+                globalOperator = itemClicked;
+                inputOutput.textContent = operand1;
+                operationFlag = true;
+            }
         }
-        inputOutput.textContent += newText;
-    });
-});
+    }
+}
 
 operationButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
-        let newNum = parseFloat(inputOutput.textContent);
-        if(e.target.id == 'clear'){
-            inputOutput.textContent = "";
-            globalOperator = undefined;
-            operand1 = undefined;
-            operand2 = undefined;
-        } else if(newNum != newNum) {
-            alert("Enter a number before trying an operation");
-        } else {
-            if(e.target.id == 'negate') {
-                inputOutput.textContent = negate(newNum);
-            } else if(e.target.id == 'one-over-x') {
-                inputOutput.textContent = oneOverX(newNum);
-            } else if(e.target.id == 'square-root') {
-                inputOutput.textContent = squareRoot(newNum);
-            } else if(e.target.id == 'equals') {
-                if(operand1 != undefined) {
-                    operand2 = newNum;
-                    total = operate(operand1, operand2, globalOperator);
-                    inputOutput.textContent = total;
-                    operationFlag = true;
-                    operand1 = undefined;
-                    operand2 = undefined;
-                }
-            } else {
-                if(operand1 == undefined) {
-                    operand1 = newNum;
-                    globalOperator = e.target.id;
-                    inputOutput.textContent = '';
-                } else {
-                    operand2 = newNum;
-                    operand1 = operate(operand1, operand2, globalOperator);
-                    globalOperator = e.target.id;
-                    inputOutput.textContent = operand1;
-                    operationFlag = true;
-                }
-            }
-        }
+        operationClicked(e);       
     });
+});
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        numberClicked(e);
+    });
+});
+
+htmlDoc.addEventListener('keydown', (e) => {
+    let keyCode = e.keyCode;
+    if((keyCode >= 96 && keyCode <= 105) || keyCode == 110) {
+        keyPressed = true;
+        numberClicked(e);
+    } else if(keyCode == 106 || keyCode == 107 || keyCode == 109 || keyCode == 111 || keyCode == 13 || keyCode == 27){
+        keyPressed = true;
+        operationClicked(e);
+    } else if(keyCode == 8){
+        let inputOutputLen = inputOutput.textContent.length;
+        inputOutput.textContent = inputOutput.textContent.slice(0, inputOutputLen - 1);
+    }
 });
 
 function add(a, b) {
